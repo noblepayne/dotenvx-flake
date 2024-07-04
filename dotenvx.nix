e580a877c31e268de3de8ae4aa642f6396b15d98
Wes Payne <noblepayne@noblepayne.com>
2024-07-04 14:17:12 -0700

    cleanup

--- dotenvx.nix
+++ dotenvx.nix
@@ -38,19 +38,16 @@ stdenv.mkDerivation (finalAttrs: {
   pkg-fetch-cache = stdenv.mkDerivation {
     pname = "dotenvx-pkg-fetch-cache";
     inherit (finalAttrs) src version;
-    outputHash =
-      if system == "x86_64-linux"
-      then "sha256-pnEXmk3z2amfZbxaDRLOASRlib6cL7C7ZuxKLaLD61Y="
-      else "";
+    outputHash = "sha256-lUjoy0njP2zmK2qATlk7SjgJW4zililqwf0KkqoWEvA=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
-    buildInputs = [cacert nodejs_18];
+    buildInputs = [cacert];
     buildPhase = ''
       runHook preBuild
       export HOME=$(mktemp -d)
       ${finalAttrs.node_modules}/node_modules/.bin/pkg-fetch
       mkdir $out
-      mv $HOME/.pkg-cache $out
+      cp -rT $HOME/.pkg-cache $out
       runHook postBuild
     '';
   };
@@ -62,24 +59,16 @@ stdenv.mkDerivation (finalAttrs: {
     buildInputs = [stdenv.cc.cc];
     buildPhase = ''
       runHook preBuild
-      mkdir -p $out/.pkg-cache
-      cp -rT .pkg-cache $out/.pkg-cache
+      mkdir -p $out
+      fetchedBin=$(find . -iname 'fetched*')
+      builtBin=$(echo $fetchedBin | sed 's/fetched/built/')
       # pretend to be "built" to avoid pkg-fetch hash checking binary after patching
-      mv $out/.pkg-cache/v3.4/{fetched-v18.5.0-linux-x64,built-v18.5.0-linux-x64}
+      mv $fetchedBin $builtBin
+      # copy to store, keeping expected path
+      cp -rT . $out
       runHook postBuild
     '';
   };
-  buildInputs = [nodejs_18];
-  #export PKG_CACHE_PATH=$HOME/.pkg-cache
-  #mkdir -p $PKG_CACHE_PATH
-  # copy downloaded file to our homedir cache
-  #cp -rT ${finalAttrs.patched-pkg-fetch-cache}/.pkg-cache $PKG_CACHE_PATH
-  # chmod -R a+rwx $PKG_CACHE_PATH
-  # patch to support nix/nixos
-  # autoPatchelf $PKG_CACHE_PATH
-  # rename to built to avoid pkg-fetch hash checking
-  #mv $PKG_CACHE_PATH/v3.4/{fetched-v18.5.0-linux-x64,built-v18.5.0-linux-x64}
-
   buildPhase = ''
     runHook preBuild
     export HOME=$(mktemp -d)
@@ -87,7 +76,7 @@ stdenv.mkDerivation (finalAttrs: {
     # restore node_modules
     cp -r ${finalAttrs.node_modules}/node_modules .
     # setup pkg-fetch cache
-    export PKG_CACHE_PATH="${finalAttrs.patched-pkg-fetch-cache}/.pkg-cache"
+    export PKG_CACHE_PATH="${finalAttrs.patched-pkg-fetch-cache}"
     # run pkg to build binary
     ./node_modules/.bin/pkg . --no-bytecode --pubic-packages "*" --public --target node18-linux-x64 --output $out/bin/dotenvx
     runHook postBuild
